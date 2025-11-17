c5038a667d97ae5fcccc10f606241d9c9d58bc58
fahad <f@gmail.com>
2025-11-17 16:07:46 +0500

    playwright config added

--- Configs/playwright.config.js
+++ Configs/playwright.config.js
@@ -54,7 +54,14 @@ export default defineConfig({
       name: "chromium",
       use: { ...devices["Desktop Chrome"] },
     },
-    // To test with other browsers, add them here
+    {
+      name: "firefox",
+      use: { ...devices["Desktop Firefox"] },
+    },
+    {
+      name: "webkit",
+      use: { ...devices["Desktop Safari"] },
+    },
   ],
 
   // webServer: {
